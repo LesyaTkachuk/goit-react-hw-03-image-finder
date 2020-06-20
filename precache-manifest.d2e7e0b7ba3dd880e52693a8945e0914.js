d833af6d30c1f7761892383b0ab724c11f537b6f
Aleksandra Tkachuk <lesyalesya9876@gmail.com>
2020-06-20 18:01:42 +0300

    Updates

---
+++ precache-manifest.d2e7e0b7ba3dd880e52693a8945e0914.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "33c2cf3cd4513f0d3752e300e8b585d6",
+    "url": "/goit-react-hw-03-image-finder/index.html"
+  },
+  {
+    "revision": "424c299dc6eae52f67b0",
+    "url": "/goit-react-hw-03-image-finder/static/css/2.78f95126.chunk.css"
+  },
+  {
+    "revision": "ac46e799b863bd6e96ac",
+    "url": "/goit-react-hw-03-image-finder/static/css/main.7e6443e7.chunk.css"
+  },
+  {
+    "revision": "424c299dc6eae52f67b0",
+    "url": "/goit-react-hw-03-image-finder/static/js/2.2121f899.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/goit-react-hw-03-image-finder/static/js/2.2121f899.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ac46e799b863bd6e96ac",
+    "url": "/goit-react-hw-03-image-finder/static/js/main.51daccd7.chunk.js"
+  },
+  {
+    "revision": "c7baa910816df6b3afca",
+    "url": "/goit-react-hw-03-image-finder/static/js/runtime-main.4877080d.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/goit-react-hw-03-image-finder/precache-manifest.e36fdcc69887c1e885fb131478b36aa6.js"
+  "/goit-react-hw-03-image-finder/precache-manifest.d2e7e0b7ba3dd880e52693a8945e0914.js"
 );
 
 self.addEventListener('message', (event) => {
